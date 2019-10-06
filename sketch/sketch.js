08d9d07269a2f3a275bdf15334123faf7527ecb3
Arshdeep Singh Badhan <Arshdeepbadhan1@gmail.com>
2019-10-06 19:21:08 -0400

    Added the p5js file with background and beginning text

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,12 +1,24 @@
+let width = 200; 
+let length = 300;
+let input;
+let button; 
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(width, length);
+  background(0);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);	
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+	//to repeat the code
+ for(let i = 1; i < 4; i++){
+text('What do you want to do?', 15, length/4);
+textSize(15);
+fill(255);
+//input = createInput();
+//input.position(width/2, (length/4) + 10);
+
+button = creatButton('Start');
+button.position(width/2, (length/4)+ 20);
+button.mousePressed(greet);
+
+ }
 }
